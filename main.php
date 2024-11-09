1e61c78ed6a59caf34d3462e09e2e8da609af77b
moh hasan <mohhasan9998@gmail.com>
2024-11-09 01:36:42 -0800

    menambahkan panduan untuk mengeksekusi server

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+// C:\project_UTS_webprograming
+// ubah ke direktori anda.
+
+// ketika di url http://localhost:8080
+// tambahkan /nama_file.php
+exec('php -S localhost:8080 -t C:\project_UTS_webprograming', $output, $status);
+?>

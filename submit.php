905d9e5792296f5972559d407a526c185183c4d2
moh hasan <mohhasan9998@gmail.com>
2024-11-09 05:03:07 -0800

    menambahkan syntax link

--- submit.php
+++ submit.php
@@ -4,7 +4,11 @@ $title = "Profil Saya";
 $name = "Moh Hasan";
 $description = "Halo! Saya seorang pengembang web yang tertarik dengan teknologi terbaru dan suka mengembangkan aplikasi yang berguna. Di waktu luang, saya menikmati membaca buku dan belajar hal baru.";
 
-
+$links = [
+    "tiktok" => "https://www.tiktok.com/@azkam_233?_t=8rCE7sFeLT8&_r=1",
+    "youtube" => "https://youtube.com/@mohhasan-ij7jg?feature=shared",
+    "instagram" => "https://www.instagram.com/yourusername"
+];
 ?>
 
 <!DOCTYPE html>

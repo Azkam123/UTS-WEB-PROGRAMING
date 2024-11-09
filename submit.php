8b173c942e048f82b7ddb5ff1c8a792fc7adf574
moh hasan <mohhasan9998@gmail.com>
2024-11-09 04:54:49 -0800

    menambahkan syntax

--- submit.php
+++ submit.php
@@ -1,4 +1,11 @@
 <!-- * untuk mengeksekusi server harus di file main.php -->
+<?php
+$title = "Profil Saya";
+$name = "Moh Hasan";
+$description = "Halo! Saya seorang pengembang web yang tertarik dengan teknologi terbaru dan suka mengembangkan aplikasi yang berguna. Di waktu luang, saya menikmati membaca buku dan belajar hal baru.";
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
